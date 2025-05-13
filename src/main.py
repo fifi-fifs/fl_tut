3fb5df250e25368cd9e6f8cc9ec6c933b5dba7ca
Sofia Canossa <sofiacanossa5@gmail.com>
2025-05-14 00:48:13 +0100

    Initial commitment - Starting code

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#  __________________
+#  Import LIBRARIES
+import flet as ft
+#  Import FILES
+#  __________________
+
+
+def main(page: ft.Page):
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    ft.app(main)
