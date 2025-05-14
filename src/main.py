a798c517f1cf465495c4348f25f0677d08aff5ea
Sofia Canossa <sofiacanossa5@gmail.com>
2025-05-14 01:13:39 +0100

    Primera aplication

--- src/main.py
+++ src/main.py
@@ -6,11 +6,25 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    pass
+    page.bgcolor = ft.Colors.BLUE_GREY_800
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.title = "Mi app"
+    texto = ft.Text("Mi primera app con Flet")
+    # page.add(texto)
+    texto2 = ft.Text("Este es un ejemplo para mi canal de Youtube")
+    # page.add(texto2)
+    # pass
 
+    def cambiar_texto(e):
+        texto2.value = "¡Suscríbete para más Tutoriales!"
+        page.update()
 
+    boton = ft.FilledButton(text="Cambiar texto", on_click=cambiar_texto)
+
+    page.add(texto, texto2, boton)
 
 
 
 if __name__ == "__main__":
     ft.app(target=main)
+    # ft.app(target=main, view=ft.WEB_BROWSER)
