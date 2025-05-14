d073938ec285bee0b0109e147b7858a38dd92bd8
Sofia Canossa <sofiacanossa5@gmail.com>
2025-05-14 16:57:10 +0100

    Add Row and Column

--- src/main.py
+++ src/main.py
@@ -7,21 +7,60 @@ import flet as ft
 
 def main(page: ft.Page):
     page.bgcolor = ft.Colors.BLUE_GREY_800
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.title = "Mi app"
-    texto = ft.Text("Mi primera app con Flet")
-    # page.add(texto)
-    texto2 = ft.Text("Este es un ejemplo para mi canal de Youtube")
-    # page.add(texto2)
-    # pass
+    page.title = "Mi app mejorada con filas y columnas"
+    
+    
+    texto1 = ft.Text(value="Texto 1", size=24, color=ft.Colors.WHITE)
+    texto2 = ft.Text(value="Texto 2", size=24, color=ft.Colors.WHITE)
+    texto3 = ft.Text(value="Texto 3", size=24, color=ft.Colors.WHITE)
+    # texto2 = ft.Text(value="Texto 2", size=18, color=ft.Colors.WHITE)
 
-    def cambiar_texto(e):
-        texto2.value = "¡Suscríbete para más Tutoriales!"
-        page.update()
+    fila_textos = ft.Row(controls=[texto1,texto2,texto3],alignment=ft.MainAxisAlignment.CENTER, spacing=50)
 
-    boton = ft.FilledButton(text="Cambiar texto", on_click=cambiar_texto)
+    boton1 = ft.FilledButton(text="Botón 1")
+    boton2 = ft.FilledButton(text="Botón 2")
+    boton3 = ft.FilledButton(text="Botón 3")
 
-    page.add(texto, texto2, boton)
+    fila_botones = ft.Row(controls=[boton1, boton2, boton3],alignment=ft.MainAxisAlignment.CENTER,spacing=50)
+
+
+    textos_columna1 = [
+        ft.Text(value="Columna 1 - Fila 1", size=18, color=ft.Colors.WHITE),
+        ft.Text(value="Columna 1 - Fila 2", size=18, color=ft.Colors.WHITE),
+        ft.Text(value="Columna 1 - Fila 3", size=18, color=ft.Colors.WHITE)
+    ]   
+
+    columna_texto1 = ft.Column(
+        controls=textos_columna1
+    )
+
+    textos_columna2 = [
+        ft.Text(value="Columna 2 - Fila 1", size=18, color=ft.Colors.WHITE),
+        ft.Text(value="Columna 2 - Fila 2", size=18, color=ft.Colors.WHITE),
+        ft.Text(value="Columna 2 - Fila 3", size=18, color=ft.Colors.WHITE)
+    ]   
+
+    columna_texto2 = ft.Column(
+        controls=textos_columna2
+        # controls=textos_columna2, alignment=ft.MainAxisAlignment.CENTER,horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=50
+    )
+
+    page.add(fila_textos, fila_botones)
+
+    fila_columns = ft.Row(
+        controls=[columna_texto1, columna_texto2], alignment=ft.MainAxisAlignment.CENTER,spacing=150,
+    )
+    page.add(fila_columns)
+
+
+
+
+    # page.add(columna_texto1)
+    # page.add(fila_textos, fila_botones, columna_texto1, columna_texto2)
+
+
+
+    # page.add(texto1,texto2,texto3)
 
 
 
